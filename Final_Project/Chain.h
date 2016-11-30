847be9d8577110e3beefb7864040fc476e154a2d
Elvenag <matperron54654@gmail.com>
2016-11-29 23:10:15 -0500

    Chain updated. Errors not working for chain. Need to discuss Gemstones getCardsPerCoin.

--- Final_Project/Chain.h
+++ Final_Project/Chain.h
@@ -13,51 +13,68 @@ using std::vector;
 
 //Chain_Base
 
-template<class T> class Chain_Base: private vector<T*>{
+template<class T> class Chain_Base: public vector<T*>{
+	T cardType;
 public:
 	Chain_Base<T>& operator+=(Card* c);
+	ostream& operator<<(ostream& os);
 };
 
-template<class Card> class Chain {};
+template<class Card> class Chain : public Chain_Base<Card> {};
 
 template<> class Chain<Ruby> : public Chain_Base<Ruby> {
 public:
 	int sell();
+	Chain(istream& is, CardFactory* cf);	
 };
 
 template<> class Chain<Quartz> : public Chain_Base<Quartz> {
 public:
 	int sell();
+	Chain(istream& is, CardFactory* cf);
+	
 };
 
 template<> class Chain<Hematite> : public Chain_Base<Hematite> {
 public:
 	int sell();
+	Chain(istream& is, CardFactory* cf);
+
 };
 
 template<> class Chain<Obsidian> : public Chain_Base<Obsidian> {
 public:
 	int sell();
+	Chain(istream& is, CardFactory* cf);
+
 };
 
 template<> class Chain < Malachite > : public Chain_Base<Malachite> {
 public:
 	int sell();
+	Chain(istream& is, CardFactory* cf);
+
 };
 
 template<> class Chain<Turquoise> : public Chain_Base<Turquoise> {
 public:
 	int sell();
+	Chain(istream& is, CardFactory* cf);
+
 };
 
 template<> class Chain<Amethyst> : public Chain_Base<Amethyst> {
 public:
 	int sell();
+	Chain(istream& is, CardFactory* cf);
+
 };
 
 template<> class Chain<Emerald> : public Chain_Base<Emerald> {
 public:
 	int sell();
+	Chain(istream& is, CardFactory* cf);
+
 };
 
 #endif
@@ -78,10 +95,22 @@ inline Chain_Base<T>& Chain_Base<T>::operator+=(Card * c)
 	return *this;
 }
 
+template<class Card>
+inline ostream & Chain_Base<Card>::operator<<(ostream & os)
+{
+	os << cardType.getName();
+	for (std::size_t i = 0; i < size(); i++) {
+		os << " ";
+		cardType.print(os);
+	}
+	return os;
+}
+
 inline int Chain<Ruby>::sell()
 {
 	return 0;
 }
+
 
 inline int Chain<Quartz>::sell() 
 {
@@ -115,5 +144,6 @@ inline int Chain<Amethyst>::sell()
 
 inline int Chain <Emerald> ::sell()
 {
+	
 	return 0;
 }
